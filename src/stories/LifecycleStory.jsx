56fb5adbfa3fb1c6b3823b256e45e7399557835b
cindy <cindy1.kim@cheilpengtai.com>
2020-10-27 18:23:15 +0900

    6장 예제  - 컴포넌트

--- src/stories/LifecycleStory.jsx
+++ src/stories/LifecycleStory.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 
-import { PageWithLoadData } from '../05/lifecycle';
+import { PageWithLoadData, PageWithLoadDataAndLoading } from '../05/lifecycle';
 
-storiesOf('Lifecycle', module).add('loadData예제', () => (
-  <PageWithLoadData loadData={() => fetch('/').then(() => 'hello')} />
-));
+storiesOf('Lifecycle', module)
+  .add('loadData예제', () => <PageWithLoadData loadData={() => fetch('/').then(() => 'hello')} />)
+  .add('로딩 메시지 예제', () => (
+    <PageWithLoadDataAndLoading loadData={() => fetch('/').then(() => 'hello')} />
+  ));
